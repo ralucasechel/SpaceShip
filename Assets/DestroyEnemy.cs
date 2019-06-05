e4f3687553f0b494b3a956c48a9e21825c939144
Raluca Sechel <raluca.sechel@pitechnologies.ro>
2019-06-05 16:32:11 +0300

    Add SI project

---
+++ Assets/DestroyEnemy.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyEnemy : MonoBehaviour
+{
+	public float tumble;
+    private GameController gameController;
+	public GameObject playerExplosion;
+	public GameObject enemyExplosion;
+
+    void Start ()
+    {
+	    GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble; 
+        GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent <GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log ("Cannot find 'GameController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.tag == "Boundary" || other.tag == "Enemy")
+        {
+            return;
+        }
+		else if (other.tag == "Player") {
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}
+       else if (other.tag == "Bolt"){
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.WinGame ();
+	   }
+	  
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public GameObject enemy;
+    public Vector3 spawnValues;
+    public Vector3 enemySpawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private bool wonGame;
+    private int score;
+
+    void Start ()
+    {
+        gameOver = false;
+		wonGame = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore ();
+        StartCoroutine (SpawnWaves ());
+    }
+
+    void Update ()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.R))
+            {
+                Application.LoadLevel (Application.loadedLevel);
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves ()
+    {
+        yield return new WaitForSeconds (startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate (hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds (spawnWait);
+            }
+            yield return new WaitForSeconds (waveWait);
+			
+			if(score >= 50) {
+				Vector3 spawnPosition = new Vector3 (enemySpawnValues.x, enemySpawnValues.y, enemySpawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (enemy, spawnPosition, spawnRotation);
+			}
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+
+			if (wonGame)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore (int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore ();
+    }
+
+    void UpdateScore ()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver ()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
+	 public void WinGame ()
+    {
+        gameOverText.text = "You won!";
+        wonGame = true;
+    }
+}
